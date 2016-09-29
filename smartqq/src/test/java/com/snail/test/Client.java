ac154923b7e892cc36c80ee5b2238c8f5112cdf2
snail <snail@LuqudeMacBook-Pro.local>
2016-09-29 22:16:32 +0800

    robot project init.

---
+++ smartqq/src/test/java/com/snail/test/Client.java
@@ -0,0 +1,16 @@
+package com.snail.test;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by snail on 9/29/16.
+ */
+public class Client {
+    private static final Logger logger = LoggerFactory.getLogger(Client.class);
+
+    public static void main(String[] args){
+        logger.info("This is a test.");
+    }
+}
