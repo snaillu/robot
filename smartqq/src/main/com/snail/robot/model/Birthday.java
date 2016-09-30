31ef8a59ba8b890703039dd3d0c5c2e3b2ea597d
luqu <luqu@meituan.com>
2016-09-30 10:22:14 +0800

    init project

---
+++ smartqq/src/main/com/snail/robot/model/Birthday.java
@@ -0,0 +1,19 @@
+package com.snail.robot.model;
+
+import lombok.Data;
+
+/**
+ * 生日
+ * @author ScienJus
+ * @date 2015/12/24.
+ */
+@Data
+public class Birthday {
+
+    private int year;
+
+    private int month;
+
+    private int day;
+
+}
---
+++ smartqq/src/main/com/snail/robot/utils/HttpUtils.java
@@ -0,0 +1,10 @@
+package com.snail.robot.utils;
+
+/**
+ * http工具类
+ *
+ * @author snail
+ * @create 2016/09/30
+ */
+public class HttpUtils {
+}
